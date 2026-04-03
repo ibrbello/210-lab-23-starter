0c89ea8f8555c506b7d9a3f775df5d5cae2eb008
Ibrahim Bello <ibrbello10@gmail.com>
2026-04-02 21:44:59 -0700

    Milestone 2 (had to take a break)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <list>
+#include <ctime>
 #include "Goat.h"
 using namespace std;
 
@@ -28,10 +29,21 @@ int main() {
     i = 0;
     while (fin1 >> colors[i++]);
     fin1.close();
-
-
+    int test = main_menu();
+    cout << test;
 
 
     return 0;
 }
 
+int main_menu() {
+    int choice;
+    cout << "**** GOAT MANAGER 3001 ****" << endl;
+    cout << "[1] Add a goat" << endl;
+    cout << "[2] Delete a goat" << endl;
+    cout << "[3] List goats" << endl;
+    cout << "[4] Quit" << endl;
+    cout << "Choice --> ";
+    cin >> choice;
+    return choice; 
+}
